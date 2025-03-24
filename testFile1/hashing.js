f19d84c424a715c2c34976c2c9e1ea56185cf329
Sidhant Pandey <sanupandey471@gmail.com>
2025-03-25 00:49:44 +0530

    added more info

--- testFile1/hashing.js
+++ testFile1/hashing.js
@@ -1,23 +1,34 @@
-// Hashing using crypto module in nodejs and finding the input that produces a hash with a prefix of entered number of zeros .
+// // Hashing using crypto module in nodejs and finding the input that produces a hash with a prefix of entered number of zeros .
 
-// Key Note
-// crypto.createHash('sha256')
-// .update()
-// .digest('hex')
+// // Key Note
+// // crypto.createHash('sha256')
+// // .update()
+// // .digest('hex')
 
-import crypto from "crypto";
+// import crypto from "crypto";
 
-function hashedInput(prefix) {
-  let input = 0;
-  while (true) {
-    let hash = crypto.createHash("sha256").update(input.toString()).digest("hex");
-    if (hash.startsWith(prefix)) {
-      return { input: input, hash: hash };
-    }
-    input++;
-  }
-}
+// function hashedInput(prefix) {
+//   let input = 0;
+//   while (true) {
+//     let hash = crypto.createHash("sha256").update(input.toString()).digest("hex");
+//     if (hash.startsWith(prefix)) {
+//       return { input: input, hash: hash };
+//     }
+//     input++;
+//   }
+// }
 
-const result = hashedInput("0000");
-console.log("Input:", result.input);
-console.log("Hash:", result.hash);
+// const result = hashedInput("0000");
+// console.log("Input:", result.input);
+// console.log("Hash:", result.hash);
+
+// We will get a bunch of bytes 
+// Encodings
+// ASCII
+// HEX
+// base64
+// base58
+
+// console.log(new TextEncoder().encode(1251))
+
+
